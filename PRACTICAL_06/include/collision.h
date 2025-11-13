35ed0e7122ab8849f52dc24d96715530576aa2ca
(Student  C00312819) Filip Svantner <C00312819@setu.ie>
2025-11-13 13:22:07 +0000

    commenting done

--- PRACTICAL_06/include/collision.h
+++ PRACTICAL_06/include/collision.h
@@ -2,28 +2,28 @@
 #include "stdlib.h"
 #include "Math.h"
 
-typedef struct CircleStruct{
+typedef struct CircleStruct{ //struct for Circle
 	
 	int x;
 	int y;
 	int radius;
 }CircleStruct;
 
-typedef struct RectangleStruct{
+typedef struct RectangleStruct{ //struct for Rectangle 
 	int x;
 	int y;
 	int width;
 	int height;
 }RectangleStruct;
 
-bool circleToCircle(CircleStruct* a, CircleStruct* b)
+bool circleToCircle(CircleStruct* a, CircleStruct* b) //Collision check through radius and distance
 {
-	int distanceX = abs(a->x - b->x);
-	int distanceY = abs(a->y - b->y);
-	int distanceSqrt = sqrt((distanceX * distanceX) + (distanceY * distanceY));
-	int difference = a->radius + b->radius;
+	int distanceX = abs(a->x - b->x); //distance on x axis
+	int distanceY = abs(a->y - b->y); // distance on y axis
+	int distanceSqrt = sqrt((distanceX * distanceX) + (distanceY * distanceY)); // straight line distance
+	int difference = a->radius + b->radius; // sum of radiuses
 	
-	if (distanceSqrt <= difference)
+	if (distanceSqrt <= difference) 
 	{
 		return true;
 	}
@@ -33,16 +33,16 @@ bool circleToCircle(CircleStruct* a, CircleStruct* b)
 	}
 }	
 
-bool valueInRange(int value, int min, int max)
+bool valueInRange(int value, int min, int max) // checks if the value is in range
 {
 	return (value >= min) && (value <= max); 
 }
 
-bool rectangleToRectangle(RectangleStruct* one, RectangleStruct* two)
+bool rectangleToRectangle(RectangleStruct* one, RectangleStruct* two) // checks if the x and y points of one square fit the other square and vice versa
 {
-	bool xOverlap = valueInRange(one->x, two->x, two->x + two->width) || valueInRange(two->x, one->x, one->x + one->width);
+	bool xOverlap = valueInRange(one->x, two->x, two->x + two->width) || valueInRange(two->x, one->x, one->x + one->width); // overlap on x axis
 	
-	bool yOverlap = valueInRange(one->y, two->y, two->y + two->height) || valueInRange(two->y, one->y, one->y + one->height);
+	bool yOverlap = valueInRange(one->y, two->y, two->y + two->height) || valueInRange(two->y, one->y, one->y + one->height); // overlap on y axis
 	
 	return xOverlap && yOverlap;
 }
